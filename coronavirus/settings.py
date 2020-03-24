2509a70f9e090dd87739dbabc9b58755082bdc97
pabloFM <pafloma@gmail.com>
2020-03-24 18:33:27 +0100

    Arreglado paquete no usado

--- coronavirus/settings.py
+++ coronavirus/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.gis',
-    'djcelery_email',
     'rest_framework',
     'rest_framework_gis',
     'base',
